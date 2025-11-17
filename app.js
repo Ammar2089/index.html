d0518f38a3f3432fbb9c304716d811ee3d4ed538
Ammar2089 <muhammadammararain10@gmail.com>
2025-11-17 18:17:16 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+console.log("hello world")
+
+let marks = prompt("please enter your marks");
+
+if (marks < 0 || marks > 100) {
+    alert("Invalid Marks")
+
+} else if (marks >= 90) {
+    alert("Congratulations! You got A* grade.");
+} else if (marks >= 80) {
+    alert("Hurray! You got A+ grade");
+} else if (marks >= 70) {
+    alert("You got A grade");
+} else if (marks >= 50) {
+    alert("You got grade C")
+ } else if (marks >= 30) {
+    alert("You got D grade ")
+ } else if (marks >= 10) {
+    alert("Better luck next time")
+ }
+for(let i=1; i<=5; i++) {
+    console.log('Count:',i);
+}
+let count= 1;
+while(count<=3){
+    console.log(Count);count++;
+}
+for(let i=1; i<=10; i++) {
+    if (i === 5) continue
+    console.log('count:', i)
+}
+function greet(){ console.log('Hello!');}
+greet();
+let sum = add(5,3);
+console.log()
+
---
+++ app2.js
@@ -0,0 +1,26 @@
+const greet =(name) => document.writein(`My name is $(name)`);
+
+function add(a,b) {
+    return a + b;
+}
+function sub(a,b) {;
+    document.writein("Hello world", "<br />");
+    document.writein("subtraction:", a - b );
+    return(a-b)
+}
+function multiply(a,b) {
+    return a * b;
+}
+function divide(a,b) {
+    return a / b
+}
+
+console.log(add(5,10))
+
+console.log(sub(5,10))
+
+console.log(multiply(5,10))
+
+console.log(divide(5,10))
+
+
